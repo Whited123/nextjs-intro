e9125baa53c0883bce9fdb712b119b8b7d4da804
kim dong young <dudtodtls24@gmail.com>
2023-06-21 21:48:41 +0900

    넥스트 js 3일차
    패치다루기. 넥스트에 관한 내용은 아니었음. 로딩스테이트를 다뤘는데, 다음강의에서 매우 중요하다고 한다.

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,10 @@
-import NavBar from "@/components/NavBar";
+import Seo from "../components/Seo";
 
 export default function Potato() {
   return (
     <div>
-      <h1>어바웃</h1>
+      <Seo title="About" />
+      <h1>About</h1>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
-import NavBar from "@/components/NavBar";
+import { useEffect, useState } from "react";
+import Seo from "../components/Seo";
+
+const API_KEY = "251564f8b7f3908e86668a55ea5cf2dd";
 
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
     <div>
-      <h1>안녕</h1>
+      <Seo title="Home" />
+      {!movies && <h4>로딩중...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
